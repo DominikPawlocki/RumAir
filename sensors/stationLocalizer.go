1c12b4f5db663c79511cb67b3f1a6454fe6eae6f
Dominik Pawlocki <d.pawlocki@onet.pl>
2019-11-24 21:52:26 +0100

    LocationIQ

--- sensors/stationLocalizer.go
+++ sensors/stationLocalizer.go
@@ -123,10 +123,34 @@ func getReverseGeocodedCities(radius int, lat float64, lon float64) (bytesRead [
 }
 
 func dddd(radius int, lat float64, lon float64) (bytesRead []byte, err error) {
+	type LocationIQReverseGeoResonse struct {
+		Address struct {
+			CityDistrict string `json:"city_district"`
+			Country      string `json:"country"`
+			CountryCode  string `json:"country_code"`
+			County       string `json:"county"`
+			HouseNumber  string `json:"house_number"`
+			Postcode     string `json:"postcode"`
+			Road         string `json:"road"`
+			State        string `json:"state"`
+			Suburb       string `json:"suburb"`
+			Town         string `json:"town"`
+		} `json:"address"`
+		Boundingbox []string `json:"boundingbox"`
+		DisplayName string   `json:"display_name"`
+		Lat         string   `json:"lat"`
+		Licence     string   `json:"licence"`
+		Lon         string   `json:"lon"`
+		OsmID       string   `json:"osm_id"`
+		OsmType     string   `json:"osm_type"`
+		PlaceID     string   `json:"place_id"`
+	}
+
 	//https://locationiq.org/v1/reverse.php?key=e281731b38bb74&lat=54.5987&lon=18.26669336&format=json
 	citiesNearbyURL := locationiqBaseApiURL + fmt.Sprintf("?callback=RumAir&radius=%v&latitude=%f&longitude=%f", radius, lat, lon)
 	fmt.Printf("ENDPOINT : %s. \n", citiesNearbyURL)
 
+	return
 }
 
 func getStationLocation(station *AirStation) (latitude float64, longitude float64, err error) {
