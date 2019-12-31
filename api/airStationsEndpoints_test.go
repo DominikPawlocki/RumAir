8c322f0520d06d08b3af0aa0e8791906ed7f2f9a
Dominik Pawlocki <Dominik_Pawlocki@epam.com>
2019-12-31 12:50:04 +0100

    endpoint test added

--- api/airStationsEndpoints_test.go
+++ api/airStationsEndpoints_test.go
@@ -1,24 +1,28 @@
 package api
 
 import (
-	"os"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"strings"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
-var stations map[string]*AirStation
-var err error
-
-func setup() {
-	stations, err = GetAllStationsCapabilities()
-}
-
-func TestMain(m *testing.M) {
-	setup()
-	code := m.Run()
-	//shutdown()
-	os.Exit(code)
-}
-
 func Test_ShowStationsSensorsCodesHandler(t *testing.T) {
-	actual := ShowStationsSensorsCodesHandler()
+	req, err := http.NewRequest("GET", "/entries", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(ShowStationsSensorsCodesHandler)
+	handler.ServeHTTP(rr, req)
+
+	assert.Equal(t, rr.Code, http.StatusOK, fmt.Sprintf("Handler returned wrong status code: got %v want %v", rr.Code, http.StatusOK))
+
+	expected197signs :=
+		"[\"Station : 00 can :  HES00_RH HES00_PA HES00_TA HES00_PM10 HES00_PM25\",\"Station : 001 can :  001FLOW 001AR_AH 001AR_RH 001AR_TIN 001AR_TDEV 001NO 001NO2 001NOX 001RH 001PA 001RAIN 001TEMP 001WD"
+	assert.True(t, strings.HasPrefix(rr.Body.String(), expected197signs), fmt.Sprintf("expected first 197 signs like above, but was %v ", rr.Body.String()))
 }
