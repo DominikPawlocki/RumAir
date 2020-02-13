7e9ce36b0009ceb0b3c4a825f75c218b1c8c75e5
Dominik Pawlocki <Dominik_Pawlocki@epam.com>
2020-02-13 14:03:34 +0100

    timeout exceeded.

--- db/connect.go
+++ db/connect.go
@@ -39,7 +39,7 @@ func PlayMongo() {
 	}
 
 	// Set a 5s timeout and ensure that it is called
-	context, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	context, cancel := context.WithTimeout(context.Background(), 15*time.Second)
 	defer cancel()
 
 	// Establish a connection to Cosmos DB
