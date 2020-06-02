a8d6a50e81fdf7d1dc9565adb7d41d61d1b8515b
Dominik Pawlocki <dominik.pawlocki@refinitiv.com>
2020-06-02 23:56:09 +0200

    swagger starts working

--- api/server.go
+++ api/server.go
@@ -18,18 +18,14 @@ func (m MockableHTTPHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	m.methodToBeCalled(w, r, m.mockableDataFetcher)
 }
 
-var orig http.Handler
+var fileServerHandler http.Handler
 
 func Main() {
 	fmt.Println("Starting a server ..")
 	myRouter := mux.NewRouter().StrictSlash(true)
-	//fs := http.FileServer(http.Dir("./swagger/")) //maybe jednak separated would be better, security
-	//orig = http.StripPrefix("/swagger/", fs)
 
-	//myRouter.PathPrefix("/swagger/").HandlerFunc()
-	//myRouter.PathPrefix("/swagger/").Handler(fs).Methods("GET")
-
-	myRouter.HandleFunc("/swagger/swagger.json", aa) //not mockable for unit testing
+	fileServerHandler = http.StripPrefix("/swagger/", http.FileServer(http.Dir("/tmp/swagger/")))
+	myRouter.HandleFunc("/swagger/swagger.json", addCorsHeadersToSwaggerJsonFileCall) //not mockable for unit testing
 
 	myRouter.HandleFunc("/stations/locate/locationIQ", LocalizeAllStationsUsingLocationIQHandler)      //not mockable for unit testing
 	myRouter.HandleFunc("/stations/{id}/locate/locationIQ", LocalizeStationUsingLocationIQHandler)     //not mockable for unit testing
@@ -49,19 +45,10 @@ func Main() {
 	log.Fatal(http.ListenAndServe(":5000", myRouter))
 }
 
-// ..
-func aa(w http.ResponseWriter, r *http.Request) {
+func addCorsHeadersToSwaggerJsonFileCall(w http.ResponseWriter, r *http.Request) {
+	// star here is antipattern. It should be the known SwaggerUI path only .
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Header().Add("Content-Type", "application/json; charset=utf-8")
-	fmt.Println("Fiel server !  ..")
 
-	//fs := http.FileServer(http.Dir("./swagger/")) //maybe jednak separated would be better, security
-	orig.ServeHTTP(w, r)
-}
-
-func addDefaultHeaders(fn http.HandlerFunc) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "*")
-		fn(w, r)
-	}
+	fileServerHandler.ServeHTTP(w, r)
 }
